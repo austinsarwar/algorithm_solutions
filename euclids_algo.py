4bbd853c8b2a7926b2af352b2dac1e0f4b896e79
austinsarwar <austin_sarwar@yahoo.com>
2023-05-03 21:30:24 -0400

    Create iterative euclid algo and test speed vs recursive

--- euclids_algo.py
+++ euclids_algo.py
@@ -4,6 +4,11 @@ their greatest common divisor, that is, the largest positive integer that evenly
 divides both m and n.
 
 '''
+m = 12345678901234567890
+n = 98765432109876543210
+
+
+import time
 # Euclids algorithm using recursion
 def euclid_algo(m,n):
     #E1. [Find remainder.] Divide m by n and let r be the remainder. (We will have 0 ≤ r < n.)
@@ -16,4 +21,27 @@ def euclid_algo(m,n):
         m = n
         n = remainder
         return euclid_algo(m,n)
-print(euclid_algo(10,2))
+    
+start_time = time.time()
+recur = euclid_algo(m,n)
+end_time = time.time()
+algo_time = end_time - start_time
+print("GCD:", recur, "Recursive Time:", algo_time )
+
+
+#print(euclid_algo(1024,645))
+def euclid_algo_iter(m,n):
+    
+    while(n > 1):
+        remainder = m % n
+        if remainder == 0:
+            break
+        m = n
+        n = remainder
+    return n
+start_time = time.time()
+iter = euclid_algo_iter(m,n)
+stop_time = time.time()
+algo_time = stop_time - start_time
+print("GCD:", iter,"Iterative Time:", algo_time)
+
